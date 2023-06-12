03446a14b798bcf5ef5eb73e25d21e29cb61e7ca
JunHo Yoon <junoyoon@gmail.com>
2023-06-13 03:27:36 +0900

    Update build.gradle.kts

--- projects/spring-app/build.gradle.kts
+++ projects/spring-app/build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
+
 plugins {
     id("org.springframework.boot") version "2.7.12"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
