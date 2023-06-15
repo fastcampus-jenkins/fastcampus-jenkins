df4adf3c70be0c510f80ad1f487a7d2d1931e8ef
JunHo Yoon <junoyoon@gmail.com>
2023-06-15 19:58:42 +0900

    Update DemoController.kt

--- projects/spring-app/src/main/kotlin/com/fastcampus/demo/controller/DemoController.kt
+++ projects/spring-app/src/main/kotlin/com/fastcampus/demo/controller/DemoController.kt
@@ -7,7 +7,13 @@ import org.springframework.web.bind.annotation.RestController
 class DemoController {
     @GetMapping("/")
     fun home(): Map<String, String> {
+        var a = 4
+        a = a+ 3
+        
         return mapOf(
+            
+            
+            
             "version" to "1.0",
             "hello" to "world"
         )
